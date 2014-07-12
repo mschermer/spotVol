cc3263b7531b972e5199822b1e6a25908bcc2b6b
mschermer <m.j.a.schermer@gmail.com>
2014-07-12 20:29:32 +0200

    Added files that were not yet on github.

---
+++ man-roxygen/references.R
@@ -0,0 +1,18 @@
+#' @references
+#' Andersen, T. G. and T. Bollerslev (1997). Intraday periodicity and volatility
+#' persistence in financial markets. Journal of Empirical Finance 4, 115-158.
+#' 
+#' Beltratti, A. and C. Morana (2001). Deterministic and stochastic methods for estimation
+#' of intraday seasonal components with high frequency data. Economic Notes 30, 205-234.
+#' 
+#' Boudt K., Croux C. and Laurent S. (2011). Robust estimation of intraweek periodicity
+#' in volatility and jump detection. Journal of Empirical Finance 18, 353-367.
+#' 
+#' Fried, Roland (2012). On the online estimation of local constant volatilities.
+#' Computational Statistics and Data Analysis 56, 3080-3090.
+#' 
+#' Kristensen, Dennis (2010). Nonparametric filtering of the realized spot volatility: 
+#' A kernel-based approach. Econometric Theory 26, 60-93.
+#' 
+#' Taylor, S. J. and X. Xu (1997). The incremental volatility information in one million
+#' foreign exchange quotations. Journal of Empirical Finance 4, 317-340.
