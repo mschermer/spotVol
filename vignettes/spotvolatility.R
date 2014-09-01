f9d12ec8d672497b8abe389ee1e865594c95b1c7
mschermer <m.j.a.schermer@gmail.com>
2014-09-01 18:00:10 +0200

    Fixed incorrect LaTeX command.

--- vignettes/spotvolatility.R
+++ vignettes/spotvolatility.R
@@ -14,17 +14,3 @@ if (before) par(mar=c(2,2,1,1))
 #  library(spotvolatility)
 #  buildVignettes("spotvolatility")
 
-## ----message=FALSE----------------------------------------
-library(spotvolatility)
-out <- spotvol(sample_prices_5min)
-class(out)
-sigma_hat <- out$spot
-
-## ----tidy=TRUE--------------------------------------------
-str(sample_prices_5min)
-out <- spotvol(sample_prices_5min)
-
-## ---------------------------------------------------------
-str(sample_returns_5min)
-out <- spotvol(sample_returns_5min)
-
